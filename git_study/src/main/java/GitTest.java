052c0a1e6be4c285f40728ac736249d0aac7abff
chenkai <569783873@qq.com>
2022-04-15 14:30:02 +0800

    hot-fix commit

--- git_study/src/main/java/GitTest.java
+++ git_study/src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("git2");
+        System.out.println("hot-fix");
     }
 }
